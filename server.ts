1c8c5914176a3a930597593a0b420d6e5a41cda2
Jose Angarita <amjose09@gmail.com>
2021-11-10 12:23:59 -0500

    Creacion de usuarios, traer la información de todos los usuarios o por filtros

--- server.ts
+++ server.ts
@@ -5,18 +5,29 @@ const main = async() =>{
 
     await conexionBD();
 
-    await userModel.create({
-        correo:"amjose09@gmail.com",
-        identificacion:"123456",
-        nombre:"Jose",
-        apellido:"Angarita"
+    // await userModel.create({
+    //     correo:"osorio@gmail.com",
+    //     identificacion:"657234",
+    //     nombre:"Jonathan",
+    //     apellido:"Garrido"
+    // })
+    // .then((user)=>{
+    //     console.log("Usuario Creado: ", user)
+    // })
+    // .catch((err)=>{
+    //     console.log("Error: ",err)
+    // })
+
+    //OBTENER TODOS LOS USUARIOS
+
+    await userModel.find()
+    .then((u)=>console.log("Todos los usuarios: ", u))
+    .catch(e=>console.error("Error obteniendo usuarios: ", e))
+
+    await userModel.find({
+        correo:'amjose09@gmail.com'
     })
-    .then((user)=>{
-        console.log("Usuario Creado: ", user)
-    })
-    .catch((err)=>{
-        console.log("Error: ",err)
-    })
+    .then(u=>console.log("Usuario filtrado: ",u))
 
 }
 
