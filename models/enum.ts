bb6ffa248ad4a4a1ef89dee0adf4265b1f99983f
Clara-giraldo <clara.giraldocardona@gmail.com>
2021-11-18 20:02:33 -0500

    Creacion modelo usuarios

---
+++ models/enum.ts
@@ -0,0 +1,17 @@
+enum Enum_Rol {
+    ESTUDIANTE = 'ESTUDIANTE',
+    LIDER = 'LIDER',
+    ADMINISTRADOR = 'ADMINISTRADOR',
+  }
+  
+  enum Enum_EstadoUsuario {
+    PENDIENTE = 'PENDIENTE',
+    AUTORIZADO = 'AUTORIZADO',
+    NO_AUTORIZADO = 'NO_AUTORIZADO',
+  }
+
+  export {
+    Enum_Rol,
+    Enum_EstadoUsuario,
+    
+  };
