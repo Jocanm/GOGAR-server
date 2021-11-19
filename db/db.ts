e7048a2057e7e2a645c67a9e725427492737d3e5
Jose Angarita <amjose09@gmail.com>
2021-11-19 12:12:54 -0500

    Probando funcionalidad creacion de usuarios y proyectos

--- db/db.ts
+++ db/db.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 const conexionBD = async() => {
 
-    return await connect("mongodb://daniel:cruzada9@cluster0-shard-00-00.klq1n.mongodb.net:27017,cluster0-shard-00-01.klq1n.mongodb.net:27017,cluster0-shard-00-02.klq1n.mongodb.net:27017/objetivos?ssl=true&replicaSet=atlas-s0fja6-shard-0&authSource=admin&retryWrites=true&w=majority")
+    return await connect("mongodb+srv://admin:admin@gogardatabase.nfqou.mongodb.net/GogarDataBase?retryWrites=true&w=majority")
     .then(()=>{
         console.log("Conexion exitosa");
     })
--- index.ts
+++ index.ts
@@ -1,18 +1,34 @@
 import conexionBD from "./db/db";
-import { Enum_tipoObjetivo } from "./models/enum";
+import { Enum_Rol, Enum_tipoObjetivo } from "./models/enum";
 import { ObjetivoModel } from "./models/objective";
+import { ProyectoModel } from "./models/proyecto";
+import { UserModel } from "./models/usuarios";
 
 const main = async () =>{
     await conexionBD();
 
-    // const object = await ObjetivoModel.create({
-    //     descripcion = "Este es el objetivo General".
-    //     tipo: Enum_tipoObjetivo.general,
-
-
+    // const user = await UserModel.create({
+    //     correo:"amjose09@gmai.com",
+    //     identificacion:"12345",
+    //     nombre:"Jose",
+    //     apellido:"Angarita",
+    //     rol:Enum_Rol.ADMINISTRADOR
     // })
 
+    // console.log("usuario: ",user)
 
+    // const proyecto = await ProyectoModel.create({
+    //     nombre:"Proyecto7",
+    //     fechaInicio: new Date("2022/11/11"),
+    //     fechaFin: new Date("2022/11/11"),
+    //     presupuesto:120000,
+    //     lider:"61971ed1895281d52e73f425"
+    // })
+    // console.log("Proyecto: ",proyecto);
+    
+    // const proyecto = await ProyectoModel.find({_id:"619720cf49d9a1c6d5816b7a"}).populate("lider")
 
-
+    // console.log(proyecto)
 }
+
+main()
