d0a6f5e2583a17eddcd2384eed4174a17216441c
Jose Angarita <amjose09@gmail.com>
2021-11-23 16:24:14 -0500

    configuración para que en el update de usuario devuelva el actualizado

--- models/usuario/resolvers.ts
+++ models/usuario/resolvers.ts
@@ -53,7 +53,7 @@ export const resolverUsuario = {
                 correo:args.correo,
                 rol:args.rol,
                 estado:args.estado
-            })
+            },{new:true})
 
             return usuarioEditado;
 
