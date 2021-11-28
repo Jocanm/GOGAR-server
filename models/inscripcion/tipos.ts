2c4c53b776f21be1be6093faf63b25e21fc13fcc
Sarge-018 <sergiorojas018@gmail.com>
2021-11-28 03:10:35 -0500

    cambios tipos inscripcion

---
+++ models/inscripcion/tipos.ts
@@ -0,0 +1,26 @@
+import { gql } from "apollo-server-express";
+
+export const tiposInscripcion = gql`
+    type Inscripcion{
+        _id: ID
+        estado: Enum_EstadoInscripcion!
+        fechaIngreso: Date!
+        fechaEgreso: Date!
+        proyecto: Proyecto
+        estudiante: Usuario!        
+    }
+
+    type Query{
+        Inscripciones: [Inscripciones]
+    }
+
+    type Mutation{
+        crearInscripcion(
+            estado: Enum_EstadoInscripcion!
+            fechaIngreso: Date            
+            proyecto: String!
+            estudiante: String! 
+        ): Inscripcion         
+    }
+
+    ;`
