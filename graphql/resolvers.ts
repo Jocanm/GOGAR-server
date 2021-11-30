ad782e870a84cb99b155868d181dad167f761f8c
Daniel Osorno <danielosorno1996@gmail.com>
2021-11-30 09:53:01 -0500

    tipos y resolvers para el modelo de objetivos del proyecto del ciclo 4

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,3 +1,8 @@
 //AQUI VAN LAS IMPORTACIONES DE TODOS LOS RESOLVERS DE LOS ESQUEMAS QUE DEBE CREAR CADA INTEGRANTE DEL GRUPO
 
-export const resolvers = [];
+
+
+
+import {resolverObjetivo} from "../models/objetivo/resolvers";
+
+export const resolvers = [resolverObjetivo];
