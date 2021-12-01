669a1a1d7a22685a517ef543dce8dd51aca02751
Sarge-018 <sergiorojas018@gmail.com>
2021-12-01 03:55:39 -0500

    correccion tipos

--- models/inscripcion/resolvers.ts
+++ models/inscripcion/resolvers.ts
@@ -1,20 +1,20 @@
-import { InscripcionModel } from "./inscripcion"
+import { InscripcionModel } from "./inscripcion";
 
 export const resolverInscripciones = {
-    Query:{
-        Inscripciones: async(parent, args) =>{
-            const inscripciones = await InscripcionModel.find();
-            return inscripciones;
-        },
+  Query: {
+    Inscripciones: async (parent, args) => {
+      const inscripciones = await InscripcionModel.find();
+      return inscripciones;
     },
-    Mutation:{
-        crearInscripcion: async (parents, args) =>{
-            const inscripcionCreada = await InscripcionModel.create({
-                estado: args.estado,                               
-                proyecto: args.proyecto,
-                estudiante: args.estudiante,
-            });
-            return inscripcionCreada;
-        }
-    }
-}
+  },
+  Mutation: {
+    crearInscripcion: async (parents, args) => {
+      const inscripcion = await InscripcionModel.create({
+        estado: args.estado,
+        proyecto: args.proyecto,
+        estudiante: args.estudiante,
+      });
+      return inscripcion;
+    },
+  },
+};
