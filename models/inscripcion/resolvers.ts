cde5b126337eb5da6334123d3be15ba0a795ae91
Sarge-018 <sergiorojas018@gmail.com>
2021-12-02 22:02:03 -0500

    mutacion inscripciones aprobadas

--- models/inscripcion/resolvers.ts
+++ models/inscripcion/resolvers.ts
@@ -4,9 +4,10 @@ export const resolverInscripciones = {
   Query: {
     Inscripciones: async (parent, args) => {
       const inscripciones = await InscripcionModel.find();
-      return inscripciones;
+      return inscripciones;      
     },
-  },
+    
+   
   Mutation: {
     crearInscripcion: async (parents, args) => {
       const inscripcion = await InscripcionModel.create({
@@ -15,6 +16,18 @@ export const resolverInscripciones = {
         estudiante: args.estudiante,
       });
       return inscripcion;
+    },
+    aprobarInscripcion: async (parent, args) => {
+      const inscripcionAprobada = await InscripcionModel.findByIdAndUpdate(
+        args.id,
+        {
+          estado: 'ACEPTADO',
+          fechaIngreso: Date.now(),
+        },
+        { new: true }
+      );
+      return inscripcionAprobada;
+
     },
   },
 };
